82b828dd94bc213c602c36e82e20cffc06bb2751
luckywolf <zhou4car@gmail.com>
2014-10-26 22:59:50 -0700

    Update 4_6_find_next_inOrder_node_BST.cpp

--- 4_6_find_next_inOrder_node_BST.cpp
+++ 4_6_find_next_inOrder_node_BST.cpp
@@ -1,5 +1,6 @@
 /*
-Write an algorithm to find the ¡®next¡¯ node (i.e., in-order successor) of a given node in a binary search tree where each node has a link to its parent.
+Write an algorithm to find the Â¡Â®nextÂ¡Â¯ node (i.e., in-order successor) of a given node in a binary search tree
+where each node has a link to its parent.
 */
 #include <iostream>
 #include <cstring>
