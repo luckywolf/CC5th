193ec81175f757499998ea0d13dd3e5d968c3656
luckywolf <zhou4car@gmail.com>
2014-11-13 23:03:22 -0800

    Update 2_6_first_node_in_circular_list.cpp

--- 2_6_first_node_in_circular_list.cpp
+++ 2_6_first_node_in_circular_list.cpp
@@ -3,7 +3,7 @@ Given a circular linked list, implement an algorithm which returns node
 at the beginning of the loop.
 
 DEFINITION
-Circular linked list: A (corrupt) linked list in which a node¡¯s next pointer 
+Circular linked list: A (corrupt) linked list in which a node's next pointer 
 points to an earlier node, so as to make a loop in the linked list.
 
 EXAMPLE
