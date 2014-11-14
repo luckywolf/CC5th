510aed7d005c1e707d4e004d5a04610adfdb0236
luckywolf <zhou4car@gmail.com>
2014-11-13 23:07:21 -0800

    Update 9_7_paint_fill.cpp

--- 9_7_paint_fill.cpp
+++ 9_7_paint_fill.cpp
@@ -1,5 +1,5 @@
 /*
-Implement the ¡°paint fill¡± function that one might see on many image editing programs. 
+Implement the "paint fill" function that one might see on many image editing programs. 
 That is, given a screen (represented by a 2-dimensional array of Colors), a point, and 
 a new color, fill in the surrounding area until you hit a border of that color.
 */
