f156ab4b5cd59344d718a033cf5c5141172eb6bb
luckywolf <zhou4car@gmail.com>
2014-11-15 11:37:48 -0800

    updated comments

--- 9_3_find_A[i]=i_element.cpp
+++ 9_3_find_A[i]=i_element.cpp
@@ -22,6 +22,11 @@ int getMagicIndex(int A[], int n) {
     return -1;
 }
 
+/*
+A[]:   -10  -5  2  2  2  3  4  7  9  12  13
+index:  0    1  2  3  4  5  6  7  8  9   10
+mid = 5, A[mid] = 3 in first round
+*/
 int getMagicIndexDupRe(int A[], int n, int start, int end) {
     if (start < 0 || end >= n || end < start) {
         return -1;
@@ -31,10 +36,16 @@ int getMagicIndexDupRe(int A[], int n, int start, int end) {
         return mid;
     }
     
+    // 1) because A[mid] != mid, we need to eliminate mid, so end is at most = mid - 1
+    // 2) if A[mid] < mid, end is at most = A[mid]
+    // combine 1) and 2), we have end = min(A[mid], mid - 1)
     int left = getMagicIndexDupRe(A, n, start, min(A[mid], mid-1));
     if (left >= 0) {
         return left;
     }
+    // 1) because A[mid] != mid, we need to eliminate mid, so start is at least = mid + 1
+    // 2) if A[mid] > mid, start is at least = A[mid]
+    // combine 1) and 2), we have start = max(A[mid], mid + 1)
     int right = getMagicIndexDupRe(A, n, max(A[mid], mid+1), end);
     return right;
 }
